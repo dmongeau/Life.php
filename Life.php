c97a16802fe53f6110eabff310f33607818bfcee
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2010-09-17 08:42:16 -0400

    5 billions years sleep

--- Life.php
+++ Life.php
@@ -1,5 +1,10 @@
 <?php
 
+//Okay so the project is LIFE.php so we want to skip all the boring pre-life part.
+//Thanks to php, we can just do a 5 billion years sleep.
+define('YEAR',3600*24*365);
+sleep(5000000000*YEAR);
+
 
 class Life {
 

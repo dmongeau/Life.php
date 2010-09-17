c84416464571c93164d4ef4e03448b1e3abc3eb9
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2010-09-17 08:47:42 -0400

    forgot set_time_limit(0)

--- Life.php
+++ Life.php
@@ -1,5 +1,7 @@
 <?php
 
+set_time_limit(0);
+
 //Okay so the project is LIFE.php so we want to skip all the boring pre-life part.
 //Thanks to php, we can just do a 5 billion years sleep.
 define('YEAR',3600*24*365);

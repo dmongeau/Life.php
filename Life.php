1c20409d640e5f219ec986cfaf8dcbe41168a3c2
David Mongeau-Petitpas <dmp@commun.ca>
2011-03-19 14:19:15 -0400

    Added doc blocks

--- Life.php
+++ Life.php
@@ -2,11 +2,21 @@
 
 set_time_limit(0);
 
-//Okay so the project is LIFE.php so we want to skip all the boring pre-life part.
-//Thanks to php, we can just do a 5 billion years sleep.
+/*
+ * Okay so the project is LIFE.php so we want to skip all the boring pre-life part.
+ * Thanks to php, we can just do a 5 billion years sleep.
+ *
+ */
 define('YEAR',3600*24*365);
 sleep(5000000000*YEAR);
 
+
+/*
+ *
+ * Life Class
+ *
+ *
+ */
 class Life {
 
 	public function __construct() {
@@ -15,6 +25,13 @@ class Life {
 
 }
 
+
+/*
+ *
+ * Atom Class
+ *
+ *
+ */
 class Atom {
 
 	public function __construct() {
